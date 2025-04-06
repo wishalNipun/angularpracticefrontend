a377575621bac89d52bc27fb68baae80ae23e35a
wishalNipun <wishal1974@gmail.com>
2025-04-06 12:01:33 +0530

    start project with create modules

---
+++ src/app/modules/auth/auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  standalone: false,
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css'
+})
+export class AuthComponent {
+
+}
